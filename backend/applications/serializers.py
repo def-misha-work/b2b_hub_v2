b3b4a20c3c0a9c3bd883a8e82318c1d535d79a34
BaronFAS <mikhail-upside@yandex.ru>
2024-06-04 20:23:19 +0300

    Create api

---
+++ backend/applications/serializers.py
@@ -0,0 +1,49 @@
+﻿from rest_framework import serializers
+
+from applications.models import (
+    Applications,
+    CompaniesPayer,
+    CompaniesRecipient,
+    TelegamUsers,
+)
+
+
+class TelegamUsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TelegamUsers
+        fields = (
+            "tg_username",
+            "tg_user_id",
+            "name",
+            "lastname",
+        )
+
+
+class CompaniesPayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompaniesPayer
+        fields = (
+            "company_name",
+            "company_inn",
+        )
+
+
+class CompaniesRecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompaniesRecipient
+        fields = (
+            "company_name",
+            "company_inn",
+        )
+
+
+class ApplicationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Applications
+        fields = (
+            "tg_user",
+            "cost",
+            "target_date",
+            "inn_payer",
+            "inn_recipient",
+        )
--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
 
-# Create your views here.
+from applications.serializers import (
+    TelegamUsersSerializer,
+    CompaniesPayerSerializer,
+    CompaniesRecipientSerializer,
+    ApplicationsSerializer,
+)
+from applications.models import (
+    Applications,
+    CompaniesPayer,
+    CompaniesRecipient,
+    TelegamUsers,
+)
+
+
+class TelegamUsersViewSet(viewsets.ModelViewSet):
+    queryset = TelegamUsers.objects.all().order_by("id")
+    serializer_class = TelegamUsersSerializer
+    authentication_classes = [BasicAuthentication]
+
+
+class CompaniesPayerViewSet(viewsets.ModelViewSet):
+    queryset = CompaniesPayer.objects.all().order_by("id")
+    serializer_class = CompaniesPayerSerializer
+    authentication_classes = [BasicAuthentication]
+
+
+class CompaniesRecipientViewSet(viewsets.ModelViewSet):
+    queryset = CompaniesRecipient.objects.all().order_by("id")
+    serializer_class = CompaniesRecipientSerializer
+    authentication_classes = [BasicAuthentication]
+
+
+class ApplicationsViewSet(viewsets.ModelViewSet):
+    queryset = Applications.objects.all().order_by("id")
+    serializer_class = ApplicationsSerializer
+    authentication_classes = [BasicAuthentication]
