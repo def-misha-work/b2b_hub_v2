4cab63082bf990fb014c171eef948b52e728beeb
BaronFAS <mikhail-upside@yandex.ru>
2024-06-09 18:06:49 +0300

    Fix nginx and const bug

--- backend/b2b_hub/settings.py
+++ backend/b2b_hub/settings.py
@@ -6,15 +6,17 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", "")
-# SECRET_KEY = "django-insecure-zybf797((ht9!l8y9col2qspk6uw@q8(jan*dqu+y863c$d4#4"
+# SECRET_KEY = os.getenv("SECRET_KEY", "")
+SECRET_KEY = "django-insecure-zybf797((ht9!l8y9col2qspk6uw@q8(jan*dqu+y863c$d4#4"
 
-# DEBUG = os.getenv("DEBUG", "false").lower() == "true"
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+# DEBUG = True
 
 ALLOWED_HOSTS = ["backend", "192.168.0.20", "localhost", "127.0.0.1", "[::1]"]
+# ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["http://192.168.0.20:8000", "http://backend", "http://*", "https://*"]
+# CSRF_TRUSTED_ORIGINS = ["http://192.168.0.20:8000", "http://backend", "http://*", "https://*"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000",]
 
 
 AUTH_USER_MODEL = "users.CustomUser"
@@ -63,24 +65,24 @@ TEMPLATES = [
 WSGI_APPLICATION = "b2b_hub.wsgi.application"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB", ""),
-#         "USER": os.getenv("POSTGRES_USER", ""),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-#         "HOST": os.getenv("DB_HOST", ""),
-#         "PORT": os.getenv("DB_PORT", 5432)
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", ""),
+        "USER": os.getenv("POSTGRES_USER", ""),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", 5432)
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
