c3d41e263f6a03b945db1b512fa29c31d64ad3c6
BaronFAS <mikhail-upside@yandex.ru>
2024-06-16 12:24:04 +0300

    Add admin panel

--- backend/applications/admin.py
+++ backend/applications/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
 
-# Register your models here.
+from applications.models import (
+    TelegamUsers,
+    CompaniesPayer,
+    CompaniesRecipient,
+    Applications,
+)
+
+
+class TelegamUsersAdmin(admin.ModelAdmin):
+    list_display = (
+        "tg_user_id",
+        "tg_username",
+        "name",
+        "lastname",
+    )
+
+
+class CompaniesPayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "tg_user",
+        "company_name_payer",
+        "company_inn_payer",
+    )
+
+
+class CompaniesRecipientAdmin(admin.ModelAdmin):
+    list_display = (
+        "tg_user",
+        "company_name_recipient",
+        "company_inn_recipient",
+    )
+
+
+class ApplicationsAdmin(admin.ModelAdmin):
+    fields = (
+        'created_at',
+        'updated_at',
+        'tg_user',
+        'cost',
+        'target_date',
+        'inn_payer',
+        'inn_recipient'
+    )
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = (
+        "id",
+        "created_at",
+        "tg_user",
+        "cost",
+        "target_date",
+        "inn_payer",
+        "inn_recipient",
+    )
+    list_filter = (
+        "target_date",
+        "tg_user",
+    )
+    search_fields = (
+        "id",
+        "cost",
+        "tg_user__tg_username",
+        "tg_user__name",
+        "tg_user__lastname",
+        "inn_payer__company_name_payer",
+        "inn_payer__company_inn_payer",
+        "inn_recipient__company_name_recipient",
+        "inn_recipient__company_inn_recipient",
+    )
+    ordering = ("created_at",)
+
+
+admin.site.register(TelegamUsers, TelegamUsersAdmin)
+admin.site.register(CompaniesPayer, CompaniesPayerAdmin)
+admin.site.register(CompaniesRecipient, CompaniesRecipientAdmin)
+admin.site.register(Applications, ApplicationsAdmin)
