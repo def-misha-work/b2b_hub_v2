a9ff7901c7f8b40c020ab0c55bc28fd90d226b7b
Михаил Приселков <mikhail-upside@yandex.ru>
2025-01-28 12:57:45 +0300

    Dev (#23)
    * Add filter
    
    ---------
    
    Co-authored-by: Михаил Приселков <54800113+BaronFAS@users.noreply.github.com>
    Co-authored-by: root <root@cv4247399.novalocal>

---
+++ backend/applications/filters.py
@@ -0,0 +1,12 @@
+﻿import django_filters
+from applications.models import Applications
+
+
+class ApplicationsFilter(django_filters.FilterSet):
+    app_status = django_filters.CharFilter(
+        field_name="app_status", lookup_expr="exact"
+    )
+
+    class Meta:
+        model = Applications
+        fields = ["app_status"]
